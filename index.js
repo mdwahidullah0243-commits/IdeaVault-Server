72e2b27bd8f33db9151228a2da3387467b672c5e
mdwahidullah0243-commits <mdwahidullah0243@gmail.com>
2026-05-19 20:15:50 +0600

    Server created with express.js

---
+++ index.js
@@ -0,0 +1,43 @@
+const express = require('express');
+const dotenv = require('dotenv');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const cors = require('cors');
+dotenv.config();
+
+const uri = process.env.MONGODB_URI;
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
+async function run() {
+    try {
+        // Connect the client to the server	(optional starting in v4.7)
+        await client.connect();
+        // Send a ping to confirm a successful connection
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+        // Ensures that the client will close when you finish/error
+        // await client.close();
+    }
+}
+run().catch(console.dir);
+
+
+app.get('/', (req, res) => {
+    res.send('Hello! IdeaVault from server');
+});
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`);
+});

aadb6e97b3fa72d1174f58d5f7919e930e05e3cf
mdwahidullah0243-commits <mdwahidullah0243@gmail.com>
2026-05-22 17:56:20 +0600

    post api created for idea route

--- index.js
+++ index.js
@@ -7,9 +7,11 @@ dotenv.config();
 const uri = process.env.MONGODB_URI;
 
 const app = express();
-const port = process.env.PORT || 5000;
+const port = process.env.PORT;
 
+// Middleware
 app.use(cors());
+app.use(express.json());
 
 const client = new MongoClient(uri, {
     serverApi: {
@@ -23,6 +25,19 @@ async function run() {
     try {
         // Connect the client to the server	(optional starting in v4.7)
         await client.connect();
+
+        const db = client.db('IdeaVault');
+        const ideasCollection = db.collection('ideas');
+
+        app.post('/idea', async (req, res) => {
+            const ideaData = req.body;
+            console.log(ideaData);
+            const result = await ideasCollection.insertOne(ideaData);
+            
+            res.send(result);
+        })
+
+
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
